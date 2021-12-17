7e83e8976eca8df69b8bdbf417a6a363f6674f3d
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2021-12-17 16:09:55 -0300

    more adjusts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,33 +3,10 @@ $(window).on('load', function () {
 
     setTimeout(function () {
         $('.download-pdf').addClass('download-pdf--active');
-        // $('.description').addClass('description--active');
+        $('.description').addClass('description--active');
     }, 3000);
-    
-    if ($(".splitAnim").length) {
-        $(".splitAnim").each(function () {
-          let $split_text = $(this);
-          $split_text.html($split_text.html().replace("&amp;", "&").replace(/./g, "<span>$&</span>").replace(/\s/g, "&nbsp;"));
-          gsap.set($split_text.children(), {
-            display: "inline-block",
-            opacity: 0,
-            y: 50,
-            transformOrigin: "50% 50%",
-          });
-        });
-    }
-    if ($(".splitAnim").length) {
-        inView('.splitAnim')
-          .on('enter', function (el) {
-            if (!el.classList.contains("sec_active")) {
-              el.classList.add("sec_active");
-              gsap.to($(el).children(), 0.25, {
-                stagger: 0.04,
-                y: 0,
-                opacity: 1,
-                delay: 0.8,
-              });
-            }
-        })
-      }
+
+    setTimeout(function () {
+      $('.legend svg').fadeIn();
+    }, 3500);
 });
